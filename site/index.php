f3bf0cd461bac2e2b4112e43cfc4769667c54b7b
Yigit Uzel <pcdt911@gmail.com>
2023-04-17 09:20:53 +0200

    1e commit

--- site/index.php
+++ site/index.php
@@ -1,16 +1,151 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WebWinkel</title>
-</head>
-
-<body>
-    <h1>Welkom bij de webwinkel</h1>
-    
-</body>
-
+<html>
+  <head>
+    <link
+      href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+      rel="stylesheet"
+      id="bootstrap-css" />
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Navbar</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style2.css" />
+  </head>
+  <body>
+    <header>
+      <img class="logo" src="/images/logo.png" />
+      <nav>
+        <ul class="nav__links">
+          <li><a href="index.php">contact</a></li>
+          <li><a href="index.php">contact</a></li>
+          <li><a href="index.php">contact</a></li>
+        </ul>
+      </nav>
+      <a class="cta" href="#"><button>Contact</button></a>
+    </header>
+    <main>
+      <img class="Frontpage" src="/images/Main.png" />
+    </main>
+    <div class="Productlijst">
+      <div class="Product">
+        <p>Gigabyte GeForce RTX 3060 Ti OC 8G</p>
+        <a href="product-detail.php"><img src="/images/3060ti.png"></a>
+        <div class="Prijs">
+        </div>
+      </div>  
+      <div class="Product">
+        <p>INNO3D GeForce RTX 3060 NVIDIA 8 GB</p>
+        <a href="product-detail.php"><img src="/images/3060.png"></a>
+      </div>
+      <div class="Product">
+        <p>Gigabyte GeForce RTX 3070 GAMING OC 8G</p>
+        <a href="product-detail.php"><img src="/images/3070.png"></a>
+      </div>
+      <div class="Product">
+        <p>MSI GeForce GTX 1660 SUPER VENTUS OC 6GB</p>  
+        <a href="product-detail.php"><img src="/images/1660Super.png"></a>
+      </div>
+    </div>
+    <div class="Productlijst">
+      <div class="Product">
+        <p>MSI GeForce GTX 1660 SUPER VENTUS OC 6GB</p>
+        <a href="product-detail.php"><img src="/images/1660Super.png"></a>
+      </div>
+      <div class="Product">
+        <p>Gigabyte GeForce RTX 3070 GAMING OC 8G</p>
+        <a href="product-detail.php"><img src="/images/3070.png"></a>
+      </div>
+      <div class="Product">
+        <p>INNO3D GeForce RTX 3060 NVIDIA 8 GB</p>
+        <a href="product-detail.php"><img src="/images/3060.png"></a>
+      </div>
+      <div class="Product">
+        <p>Gigabyte GeForce RTX 4090 OC 24G</p>
+        <a href="product-detail.php"><img src="/images/4090.png"></a>
+      </div>
+    </div>
+    <br><br><br><br>
+    <footer>
+      <footer
+        class="section footer-classic context-dark bg-image"
+        style="background: #353434">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4">
+                <a class="brand" href="index.html">
+                  <br><br><br><br>
+                <p>
+                  We are an award-winning creative agency, dedicated to the best
+                  result in web design, promotion, business consulting, and
+                  marketing.
+                </p>
+                <p class="rights">
+                  <span>© </span><span class="copyright-year">2023</span
+                  ><span> </span><span>Company</span><span> - </span
+                  ><span>All Rights Reserved.</span>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5>Contacts</h5>
+              <dl class="contact-list">
+                <dt>Address:</dt>
+                <dd>bertha von suttnerstraat 10, 2070LM</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>email:</dt>
+                <dd><a href="mailto:#">pcdt911@gmail.com</a></dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>phones:</dt>
+                <dd>
+                  <a href="tel:#">06-33-52-80-33</a>
+                </dd>
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-3">
+              <h5>Links</h5>
+              <ul class="nav-list">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Pricing</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters social-container">
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-facebook"></span
+              ><span>Facebook</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-instagram"></span
+              ><span>Instagram</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-twitter"></span><span>Twitter</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-youtube-play"></span
+              ><span>Youtube</span></a
+            >
+          </div>
+        </div>
+      </footer>
+    </footer>
+  </body>
 </html>
---
+++ site/product-detail.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link
+      href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+      rel="stylesheet"
+      id="bootstrap-css" />
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Navbar</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+<body>
+    <header>
+        <img class="logo" src="/images/logo.png" />
+        <nav>
+          <ul class="nav__links">
+            <li><a href="index.php">contact</a></li>
+            <li><a href="index.php">contact</a></li>
+            <li><a href="index.php">contact</a></li>
+          </ul>
+        </nav>
+        <a class="cta" href="#"><button>Contact</button></a>
+      </header>
+    <div class="Webshop">
+     <a href="product-detail.php"><img src="/images/web4090.png"></a>
+        <div class="spec">
+            <style>
+                table {
+                  font-family: arial, sans-serif;
+                  border-collapse: collapse;
+                  width: 110%;
+                }
+                
+                td, th {
+                  border: 1px solid #000000;
+                  text-align: left;
+                  padding: 1px;
+                }
+                td{
+                    background-color: #ffffff;
+                }
+                tr:nth-child(even) {
+                  background-color: #ffffff;
+                }
+                th{
+                    background-color: #696969;
+                }
+                </style>
+                </head>
+                <body>
+                <table>
+                  <tr>
+                    <th>Belangrijkste</th>
+                    <th>specificaties</th>
+                  </tr>
+                  <tr>
+                    <td>Merk</td>
+                    <td>Gigabyte</td>
+                  </tr>
+                  <tr>
+                    <th>Graphics Engine </th>
+                    <th>GeForce RTX 4070ti</th>
+                  </tr>
+                  <tr>
+                    <td>Videogeheugn</td>
+                    <td>12 GB</td>
+                  </tr>
+                  <tr>
+                    <th>CUDA cores</th>
+                    <th>7680</th>
+                  </tr>
+                  <tr>
+                    <td>VGA Geheugen type</td>
+                    <td>GDDR6X</td>
+                  </tr>
+                  <tr>
+                    <th>DLSS versie</th>
+                    <th>DLSS 3</th>
+                  </tr>
+                  <tr>
+                    <td>EAN</td>
+                    <td>4719331312565</td>
+                  </tr>
+                  <tr>
+                    <th>Vendorcode</th>
+                    <th>GV-N407TGAMING 0C-12GD</th>
+                  </tr>
+                  <tr>
+                    <td>Garantie</td>
+                    <td>36 Maanden</td>
+                  </tr>
+                </table>
+                <br>
+                <table>
+                  <tr>
+                    <td>Alle </td>
+                    <td>specificaties</td>
+                  </tr>
+                  <tr>
+                    <th>Kleur product</th>
+                    <th>Grijs, Zwart</th>
+                  </tr>
+                  <tr>
+                    <td>Type koeling</td>
+                    <td>Luchtkoeling</td>
+                  </tr>
+                  <tr>
+                    <th>Minimale voeding</th>
+                    <th>750 Watt</th>
+                  </tr>
+                  <tr>
+                    <td>Geheugen interface</td>
+                    <td>192 bit</td>
+                  </tr>
+                  <tr>
+                    <th>Breedte</th>
+                    <th>140mm</th>
+                  </tr>
+                  <tr>
+                    <td>lengte</td>
+                    <td>336mm</td>
+                  </tr>
+                  <tr>
+                    <th>Hoogte</th>
+                    <th>58mm</th>
+                  </tr>
+                  <tr>
+                    <td>DisplayPort uit</td>
+                    <td>3x</td>
+                  </tr>
+                  <tr>
+                    <th>HDMI uit </th>
+                    <th>1x</th>
+                  </tr>
+                  <tr>
+                    <td>PCI Express versie</td>
+                    <td>PCI-E 4.0</td>
+                  </tr>
+                  <tr>
+                    <th>DirectX versie</th>
+                    <th>12.0</th>
+                  </tr>
+                  <tr>
+                    <td>GPU snelheid (base)</td>
+                    <td>2640</td>
+                  </tr>
+                  <tr>
+                    <th>OpenGL versie</th>
+                    <th>4.6</th> 
+                  </tr>
+                </table>
+         </div>
+    </div>  
+    <br><br><br><br>
+    <footer>
+      <footer
+        class="section footer-classic context-dark bg-image"
+        style="background: #353434">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-md-4 col-xl-5">
+              <div class="pr-xl-4">
+                <a class="brand" href="index.html">
+                  <br><br><br><br>
+                <p>
+                  We are an award-winning creative agency, dedicated to the best
+                  result in web design, promotion, business consulting, and
+                  marketing.
+                </p>
+                <p class="rights">
+                  <span>© </span><span class="copyright-year">2023</span
+                  ><span> </span><span>Company</span><span> - </span
+                  ><span>All Rights Reserved.</span>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <h5>Contacts</h5>
+              <dl class="contact-list">
+                <dt>Address:</dt>
+                <dd>bertha von suttnerstraat 10, 2070LM</dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>email:</dt>
+                <dd><a href="mailto:#">pcdt911@gmail.com</a></dd>
+              </dl>
+              <dl class="contact-list">
+                <dt>phones:</dt>
+                <dd>
+                  <a href="tel:#">06-33-52-80-33</a>
+                </dd>
+              </dl>
+            </div>
+            <div class="col-md-4 col-xl-3">
+              <h5>Links</h5>
+              <ul class="nav-list">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contacts</a></li>
+                <li><a href="#">Pricing</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters social-container">
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-facebook"></span
+              ><span>Facebook</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-instagram"></span
+              ><span>Instagram</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-twitter"></span><span>Twitter</span></a
+            >
+          </div>
+          <div class="col">
+            <a class="social-inner" href="#"
+              ><span class="icon mdi mdi-youtube-play"></span
+              ><span>Youtube</span></a
+            >
+          </div>
+        </div>
+      </footer>
+    </footer>
+</body>
+</html>
